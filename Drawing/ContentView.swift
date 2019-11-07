4487fd012f0f7afc10ec88612113a03916df386c
Issac Penn <pbbsignup@outlook.com>
2019-11-07 20:34:17 +0800

    Day 44 done

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,20 +9,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-//                .stroke(Color.red, lineWidth: 1)
-                .fill(Color.red, style: FillStyle(eoFill: true, antialiased: true))
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            Slider(value: $colorCycle)
         }
     }
 }
@@ -80,6 +73,36 @@ struct Flower: Shape {
             path.addPath(rotatedPetal)
         }
         return path
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { step in
+                Circle()
+                    .inset(by: CGFloat(step))
+//                    .strokeBorder(self.color(for: step, brightness: 1), lineWidth: 2)
+                .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                    self.color(for: step, brightness: 1),
+                    self.color(for: step, brightness: 0.5),
+                ]),startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
